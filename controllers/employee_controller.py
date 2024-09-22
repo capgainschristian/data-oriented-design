9f62607809a1384d39d54965a5172dd8959ff1b6
Christian Graham <capgainschristian@gmail.com>
2024-09-22 10:53:39 -0400

    added total employees, avg salary, highest + lowest salaries

--- controllers/employee_controller.py
+++ controllers/employee_controller.py
@@ -8,3 +8,28 @@ class EmployeeDataController:
             employees.append(self.employee_data.names[i])
 
         return employees
+    
+    def get_total_employees(self):
+        total = 0
+        for i in range(len(self.employee_data.names)):
+            total += 1
+        return total
+    
+    def get_average_salary(self):
+        avg_total = 0
+        for i in range(len(self.employee_data.salaries)):
+            avg_total += self.employee_data.salaries[i]
+        return { "average salary": avg_total}
+    
+    def find_highest_lowest_salary(self):
+        lowest = 0
+        highest = 0
+        for i in range(len(self.employee_data.salaries)):
+            if self.employee_data.salaries[i] < lowest or lowest == 0:
+                lowest = self.employee_data.salaries[i]
+            elif self.employee_data.salaries[i] > highest:
+                highest = self.employee_data.salaries[i]
+        return {
+            "lowest salary": lowest,
+            "highest salary": highest,
+        }
--- main.py
+++ main.py
@@ -7,6 +7,10 @@ controller = EmployeeDataController(employee_data)
 employee_data.add_employee(1, "Christian", 200000, "IT", "wild wings")
 employee_data.add_employee(2, "Bob", 60000, "HR", "pizza")
 employee_data.add_employee(3, "Michael", 125000, "Sales", "pho")
+employee_data.add_employee(4, "John", 80000, "HR", "pasta")
+employee_data.add_employee(5, "Randy", 73000, "IT", "paneer")
+employee_data.add_employee(6, "Lisa", 110000, "Sales", "wild wings")
+employee_data.add_employee(7, "Rachael", 175000, "Sales", "steak")
 
 employee_data.update_employee(2, name="John")
 
@@ -21,3 +25,6 @@ christian = employee_data.employee_name_lookup('Christian')
 print(christian)
 
 print(controller.get_all_employees())
+print(controller.get_total_employees())
+print(controller.get_average_salary())
+print(controller.find_highest_lowest_salary())
