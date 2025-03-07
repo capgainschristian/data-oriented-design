31b9f84054d92a94acc727afe8f0a8fb43dcd563
Christian Graham <capgainschristian@gmail.com>
2025-03-06 22:15:42 -0500

    added first test

--- tests/test_employee_data.py
+++ tests/test_employee_data.py
@@ -1,7 +1,19 @@
 import unittest
 
-from ..employee_data.employee_data import EmployeeData
+from employee_data.employee_data import EmployeeData
+
 
 class TestEmployeeData(unittest.TestCase):
     def setUp(self):
         self.db = EmployeeData()
+
+    def test_add_employee(self):
+        self.db.add_employee(1, "Alice", 60000, "IT", "Pizza")
+        self.assertEqual(len(self.db), 1)
+        self.assertEqual(self.db.lookup_employee(1), {
+            "id": 1,
+            "name": "Alice",
+            "salary": 60000,
+            "department": "IT",
+            "favorite food": "Pizza",
+        })
